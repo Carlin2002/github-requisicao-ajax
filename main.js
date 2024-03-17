e6dc2b68836444f4d676af68f210156c96b5a576
Carlin2002 <112793747+Carlin2002@users.noreply.github.com>
2024-03-17 17:38:42 -0300

    implementação catch

--- main.js
+++ main.js
@@ -11,15 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     fetch(endpoint)
     .then(function(res) {
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
         return res.json();
     })
     .then(function(json) {
-        avatarElement.src = json.avatar_url
-        nameElement.innerText = json.name
+        avatarElement.src = json.avatar_url;
+        nameElement.innerText = json.name;
         usernameElement.innerText = `@${json.login}`;
-        reposElement.innerText = json.public_repos
-        followersElement.innerText = json.followers
-        followingElement.innerText = json.following
-        linkElement.href = json.html_url
+        reposElement.innerText = json.public_repos;
+        followersElement.innerText = json.followers;
+        followingElement.innerText = json.following;
+        linkElement.href = json.html_url;
     })
-})
+    .catch(function(error) {
+        alert('Ocorreu um erro, tente novamente!');
+        console.error('Erro na solicitação fetch:', error);
+    });
+});
