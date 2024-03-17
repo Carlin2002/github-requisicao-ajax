374823cce3c32755c085b3acf816bfcc0ef0cb20
Carlin2002 <112793747+Carlin2002@users.noreply.github.com>
2024-03-17 11:21:17 -0300

    second commit

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const usernameElement = document.querySelector('#username');
     const reposElement = document.querySelector('#repos');
     const followersElement = document.querySelector('#followers');
-    const FollowingElement = document.querySelector('#following');
+    const followingElement = document.querySelector('#following');
     const linkElement = document.querySelector('#link');
 
     fetch(endpoint)
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
         usernameElement.innerText = `@${json.login}`;
         reposElement.innerText = json.public_repos
         followersElement.innerText = json.followers
-        FollowingElement.innerText = json.following
+        followingElement.innerText = json.following
         linkElement.href = json.html_url
     })
 })
